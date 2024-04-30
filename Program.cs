4434342997396006abda0aacc5eec09ac4389f75
Gustavo Dias Monteiro <gussmonteiro@gmail.com>
2024-04-30 07:45:32 -0300

    Program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 using estacionamento.Data;
+using estacionamento.Models.Enums;
 using estacionamento.Services;
 using estacionamento.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +24,34 @@ builder.Services.AddSwaggerGen(c=>
         Example = new OpenApiString("30/04/2024-12:00:00.000") 
     });
 });
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    var key = Encoding.UTF8.GetBytes("50ed68fa66ba4bb6a56684ae508e2347");
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = "omonteirox",
+        ValidAudience = "AudienciaVenenosa",
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+builder.Services.AddAuthorization(opt =>
+{
+    opt.AddPolicy("AdminOnly", policy =>
+             policy.RequireClaim("TypeUser", TypeUserEnum.ADMIN.ToString()));
+
+    opt.AddPolicy("ManagerOnly", policy =>
+        policy.RequireClaim("TypeUser", TypeUserEnum.MANAGER.ToString()));
+
+    opt.AddPolicy("UserOnly", policy =>
+        policy.RequireClaim("TypeUser", TypeUserEnum.USER.ToString()));
+});
+
 builder.Services.AddControllers()
            .AddJsonOptions(options =>
            {
@@ -26,6 +60,7 @@ builder.Services.AddControllers()
 builder.Services.AddScoped<VehicleService>();
 builder.Services.AddScoped<EstablishmentService>();
 builder.Services.AddScoped<InOutEstablishmentService>();
+builder.Services.AddScoped<UserService>();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseNpgsql(@"Host=localhost;Port=5432;Database=estacionamento;Username=postgres;Password=admin123;");
@@ -39,6 +74,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.UseHttpsRedirection();
 app.UseSwagger();
