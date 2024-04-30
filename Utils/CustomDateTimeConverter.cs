cfcb2460b678f4c5c0656e716e9808872199dc32
Gustavo Dias Monteiro <gussmonteiro@gmail.com>
2024-04-29 22:58:37 -0300

    utils

---
+++ Utils/CustomDateTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace estacionamento.Utils
+{
+    public class CustomDateTimeConverter : JsonConverter<DateTime>
+    {
+        private const string DateFormat = "dd/MM/yyyy-HH:mm:ss";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string dateString = reader.GetString();
+            if (DateTime.TryParseExact(dateString, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            {
+                return date;
+            }
+
+            throw new JsonException($"Cannot parse date: {dateString}");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            string formattedDate = value.ToString(DateFormat, CultureInfo.InvariantCulture);
+            writer.WriteStringValue(formattedDate);
+        }
+    }
+}
