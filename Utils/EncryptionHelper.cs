9cfb9e08dee848d24748fba89e715437e9e98c38
Gustavo Dias Monteiro <gussmonteiro@gmail.com>
2024-04-30 07:45:20 -0300

    TokenJWT

---
+++ Utils/EncryptionHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace estacionamento.Utils
+{
+    public class EncryptionHelper
+    {
+        private static byte[] key = Encoding.UTF8.GetBytes("6293dbaf37ec4c10b5dd36b7521fbfxc");
+        private static byte[] iv = Encoding.UTF8.GetBytes("6293dbaf37ec4c10");
+
+        public static string Encrypt(string input)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    var bytes = Encoding.UTF8.GetBytes(input);
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string encryptedText)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream(Convert.FromBase64String(encryptedText)))
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (var sr = new StreamReader(cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Utils/TokenHelper.cs
@@ -0,0 +1,39 @@
+﻿using estacionamento.Models.Enums;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace estacionamento.Utils
+{
+    public class TokenHelper
+    {
+        public static string GenerateToken(string username, TypeUserEnum role)
+        {
+            var key = Encoding.UTF8.GetBytes("50ed68fa66ba4bb6a56684ae508e2347");
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim("TypeUser", role.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+                Issuer = "omonteirox",
+                Audience = "AudienciaVenenosa"
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
